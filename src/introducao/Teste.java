431387d1ee452242ea77e7a01e73ce2e08a3a006
Rosifaarias <rosilene.farias00@gmail.com>
2024-12-04 08:40:54 -0300

    Teste

---
+++ src/introducao/Teste.java
@@ -0,0 +1,20 @@
+package introducao;
+
+
+
+public class Teste {
+
+    public static void main(String[] args) {
+    
+    	boolean estaVivo = false;
+
+  	if (1 > 5) {
+  	  System.out.println("está vivo.");
+    
+  	} else if (5 >1) {
+  		
+  		 System.out.println("não está vivo.");
+  	}
+  	  
+    }
+}
